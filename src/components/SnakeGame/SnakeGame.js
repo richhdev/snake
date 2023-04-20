1b963d0e2a8029daac838cc5cd686ea58c3403d5
richhdev <79666708+richhdev@users.noreply.github.com>
2023-04-20 16:50:20 +1000

    wip

--- src/components/SnakeGame/SnakeGame.js
+++ src/components/SnakeGame/SnakeGame.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 import AppleSvg from "./assets/apple.svg";
 import AvocadoSvg from "./assets/avocado.svg";
 import BurgerSvg from "./assets/burger.svg";
@@ -12,6 +12,24 @@ import Button from "../Button";
 import Text from "../Text";
 
 const SnakeGame = (props) => {
+  // faces
+  const face1 = useRef();
+  const face2 = useRef();
+  const face3 = useRef();
+  const face4 = useRef();
+  const face5 = useRef();
+  const face6 = useRef();
+
+  const [activeFace, setActiveFace] = useState(1);
+  const activeFaceMap = {
+    1: "none",
+    2: "rotateY(-90deg)",
+    3: "rotateY(-180deg)",
+    4: "rotateY(-270deg)",
+    5: "rotateX(-90deg)",
+    6: "rotateX(90deg)",
+  };
+
   // game
   const [gameState, setGameState] = useState("begin");
   const [gameOver, setGameOver] = useState(false);
@@ -19,7 +37,7 @@ const SnakeGame = (props) => {
   const gridHeight = 21;
   const requestAnimationFrameRef = useRef();
   const previousTimeRef = useRef(0);
-  const gameSpeed = 200; // game refresh rate in milliseconds
+  const gameSpeed = 100; // game refresh rate in milliseconds
   const snakeWrap = true; // should the snake die when it hits a all or die
 
   // direction
@@ -29,16 +47,16 @@ const SnakeGame = (props) => {
 
   // snake
   const initialSnake = [
-    { x: 3, y: 9, direction: "right" },
-    { x: 2, y: 9, direction: "down" },
-    { x: 2, y: 8, direction: "down" },
-    { x: 2, y: 7, direction: "down" },
+    { f: 1, x: 3, y: 9, direction: "right" },
+    { f: 1, x: 2, y: 9, direction: "down" },
+    { f: 1, x: 2, y: 8, direction: "down" },
+    { f: 1, x: 2, y: 7, direction: "down" },
   ];
   const [snakeBody, setSnakeBody] = useState(initialSnake);
   const snakeGrowAmount = 2; // how much does the snake grow when it eats food
 
   // food
-  const initalFood = { x: 11, y: 9 };
+  const initalFood = { f: 1, x: 11, y: 9 };
   const [foodPos, setFoodPos] = useState(initalFood);
 
   // game loop
@@ -126,7 +144,7 @@ const SnakeGame = (props) => {
   function updateSnake() {
     let newArr = snakeBody;
 
-    // update position of each snake segment
+    // update position of each snake segment (excluding head)
     for (let i = snakeBody.length - 2; i >= 0; i--) {
       newArr[i + 1] = { ...snakeBody[i] };
     }
@@ -165,6 +183,107 @@ const SnakeGame = (props) => {
     if (snakeWrap) {
       const minVal = 0;
       const maxVal = axis === "x" ? gridWidth + 1 : gridHeight + 1;
+      let newF = newArr[0].f;
+
+      // change face - horizontal //////
+      // face 1 > 2
+      if (newArr[0].x === maxVal && newArr[0].f === 1) {
+        console.log("1 > 2");
+        newF = 2;
+      }
+
+      // face 2 < 1
+      if (newArr[0].x === minVal && newArr[0].f === 2) {
+        console.log("2 < 1");
+        newF = 1;
+      }
+
+      // face 2 > 3
+      if (newArr[0].x === maxVal && newArr[0].f === 2) {
+        console.log("2 > 3");
+        newF = 3;
+      }
+
+      // face 3 < 2
+      if (newArr[0].x === minVal && newArr[0].f === 3) {
+        console.log("3 < 2");
+        newF = 2;
+      }
+
+      // face 3 > 4
+      if (newArr[0].x === maxVal && newArr[0].f === 3) {
+        console.log("3 > 4");
+        newF = 4;
+      }
+
+      // face 4 < 3
+      if (newArr[0].x === minVal && newArr[0].f === 4) {
+        console.log("4 < 3");
+        newF = 3;
+      }
+
+      // face 4 > 1
+      if (newArr[0].x === maxVal && newArr[0].f === 4) {
+        console.log("4 > 1");
+        newF = 1;
+      }
+
+      // face 1 < 4
+      if (newArr[0].x === minVal && newArr[0].f === 1) {
+        console.log("1 < 4");
+        newF = 4;
+      }
+
+      ///
+
+      // change face - vertical //////
+
+      // face 1 /\ 5
+      if (newArr[0].y === minVal && newArr[0].f === 1) {
+        console.log("1 /\\ 5");
+        newF = 5;
+      }
+
+      // face 1 \/ 6
+      if (newArr[0].y === maxVal && newArr[0].f === 1) {
+        console.log("1 \\/ 6");
+        newF = 6;
+      }
+
+      // face 5 \/ 1
+      if (newArr[0].y === maxVal && newArr[0].f === 5) {
+        console.log("5 \\/ 1");
+        newF = 1;
+      }
+
+      // face 6 /\ 1
+      if (newArr[0].y === minVal && newArr[0].f === 6) {
+        console.log("6 /\\ 1");
+        newF = 1;
+      }
+
+      ////
+
+      // face 2 /\ 5 <
+      if (newArr[0].y === minVal && newArr[0].f === 2) {
+        console.log("2 /\\ 5 <");
+        newF = 5;
+        // face5.current.style.transform = "rotate(-90deg)";
+        // face5.current.style.transition = "transform 100ms ease";
+
+        // const x = newArr[0].x;
+        // const y = newArr[0].y;
+
+        // newArr[0].y = 21 - x;
+        // newArr[0].x = 21;
+
+        // setDirection("left");
+      }
+
+      newArr[0].f = newF;
+
+      ///////////////
+
       if (newArr[0][axis] === minVal) {
         newArr[0][axis] = maxVal - 1;
       } else if (newArr[0][axis] === maxVal) {
@@ -196,7 +315,6 @@ const SnakeGame = (props) => {
     if (snakeEatItself) {
       setGameOver(true);
     }
-
     setSnakeBody([...newArr]);
   }
 
@@ -270,13 +388,127 @@ const SnakeGame = (props) => {
           </GameModal>
         )}
 
-        <GameBoard width={gridWidth} height={gridHeight}>
-          <Snake body={[...snakeBody]} />
-          <Food pos={foodPos} />
-        </GameBoard>
+        <div>
+          <div>
+            <GameBoard ref={face5} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={5} />
+              <Food pos={foodPos} />
+            </GameBoard>
+          </div>
+          <div style={{ display: "flex" }}>
+            <GameBoard ref={face1} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={1} />
+              <Food pos={foodPos} />
+            </GameBoard>
+            <GameBoard ref={face2} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={2} />
+              <Food pos={foodPos} />
+            </GameBoard>
+            <GameBoard ref={face3} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={3} />
+              <Food pos={foodPos} />
+            </GameBoard>
+            <GameBoard ref={face4} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={4} />
+              <Food pos={foodPos} />
+            </GameBoard>
+          </div>
+          <div>
+            <GameBoard ref={face6} width={gridWidth} height={gridHeight}>
+              <Snake body={[...snakeBody]} face={6} />
+              <Food pos={foodPos} />
+            </GameBoard>
+          </div>
+        </div>
+
+        <div>
+          <Scene>
+            <Cube style={{ transform: activeFaceMap[activeFace] }}>
+              <GameBoard
+                ref={face5}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face5"
+              >
+                <Snake body={[...snakeBody]} face={5} />
+                <Food pos={foodPos} />
+              </GameBoard>
+              <GameBoard
+                ref={face1}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face1"
+              >
+                <Snake body={[...snakeBody]} face={1} />
+                <Food pos={foodPos} />
+              </GameBoard>
+              <GameBoard
+                ref={face2}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face2"
+              >
+                <Snake body={[...snakeBody]} face={2} />
+                <Food pos={foodPos} />
+              </GameBoard>
+              <GameBoard
+                ref={face3}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face3"
+              >
+                <Snake body={[...snakeBody]} face={3} />
+                <Food pos={foodPos} />
+              </GameBoard>
+              <GameBoard
+                ref={face4}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face4"
+              >
+                <Snake body={[...snakeBody]} face={4} />
+                <Food pos={foodPos} />
+              </GameBoard>
+              <GameBoard
+                ref={face6}
+                width={gridWidth}
+                height={gridHeight}
+                className="face face6"
+              >
+                <Snake body={[...snakeBody]} face={6} />
+                <Food pos={foodPos} />
+              </GameBoard>
+            </Cube>
+          </Scene>
+
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <div
+            style={{
+              width: "400px",
+              display: "grid",
+              grid: "1fr / repeat(6,1fr)",
+              gap: "4px",
+            }}
+          >
+            <Button onClick={() => setActiveFace(1)}>1</Button>
+            <Button onClick={() => setActiveFace(2)}>2</Button>
+            <Button onClick={() => setActiveFace(3)}>3</Button>
+            <Button onClick={() => setActiveFace(4)}>4</Button>
+            <Button onClick={() => setActiveFace(5)}>5</Button>
+            <Button onClick={() => setActiveFace(6)}>6</Button>
+          </div>
+        </div>
       </Outer>
-      {/* <pre>{snakeBody.map((item) => `x:${item.x} y: ${item.y} \n`)}</pre>
-      <p>gane over: {gameOver ? "true" : "false"}</p>
+
+      <pre>
+        {snakeBody.map((item) => `f:${item.f} x:${item.x} y: ${item.y} \n`)}
+      </pre>
+
+      {/* <p>gane over: {gameOver ? "true" : "false"}</p>
       <p>direction: {direction}</p> */}
     </>
   );
@@ -292,29 +524,36 @@ const GlobalStyle = createGlobalStyle``;
 
 // Game
 const GameBoard = styled.div.attrs((props) => {
-  return {
-    style: {
-      grid: `repeat(${props.width}, 1fr) / repeat(${props.height}, 1fr)`,
-    },
-  };
+  // return {
+  //   style: {
+  //     grid: `repeat(${props.width}, 1fr) / repeat(${props.height}, 1fr)`,
+  //   },
+  // };
 })`
-  background-color: ${(props) => (props.theme.isDark ? "black" : "white")};
+  /* background-color: ${(props) =>
+    props.theme.isDark ? "black" : "white"}; */
   width: 100vw;
   aspect-ratio: 1/1;
   max-width: 768px;
-  border-radius: 6px;
+  /* border-radius: 6px; */
 
   display: grid;
+  grid: repeat(21, 1fr) / repeat(21, 1fr);
+
+  width: 250px;
+  border: 1px solid green;
 `;
 
 // Snake /////////////////////////////
 const Snake = (props) => {
   return (
     <>
-      {props.body.map(({ x, y, direction }, i) => {
+      {props.body.map(({ f, x, y, direction }, i) => {
         const isHead = i == 0;
         const isBody = i != 0 && i != props.body.length - 1;
         const isTail = i == props.body.length - 1;
+
+        if (props.face !== f) return;
 
         return (
           <SnakeSegment key={`snake-segment-${i}`} x={x} y={y}>
@@ -480,6 +719,25 @@ function getRandomNumBetween(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+function closestNumber(n, m) {
+  // find the quotient
+  let q = parseInt(n / m);
+
+  // 1st possible closest number
+  let n1 = m * q;
+
+  // 2nd possible closest number
+  let n2 = n * m > 0 ? m * (q + 1) : m * (q - 1);
+
+  // if true, then n1 is the
+  // required closest number
+  if (Math.abs(n - n1) < Math.abs(n - n2)) return n1;
+
+  // else n2 is the required
+  // closest number
+  return n2;
+}
+
 // game state ////
 const Outer = styled.div`
   position: relative;
@@ -492,8 +750,72 @@ const GameModal = styled.div`
   height: 100%;
   background: ${(props) =>
     props.theme.isDark ? `rgba(0, 0, 0, 0.9)` : `rgba(255, 255, 255, 0.9)`};
-  border-radius: 6px;
+  /* border-radius: 6px; */
 
   display: grid;
   place-items: center;
+`;
+
+////////////////////////////////////////////////////////////////////////////////
+
+const size = 250;
+const Scene = styled.div`
+  width: ${size}px;
+  height: ${size}px;
+  z-index: 1000;
+  perspective: calc(400 * 1px);
+  perspective-origin: 50% 50%;
+`;
+
+const rotate = keyframes`
+
+  from {
+    transform: translateZ(-100px) rotateY(calc(0 * 1deg));
+  }
+
+  to {
+    transform: translateZ(-100px) rotateY(calc(-360 * 1deg));
+  }
+  
+
+`;
+
+const Cube = styled.div`
+  width: ${size}px;
+  height: ${size}px;
+  transform-style: preserve-3d;
+  position: relative;
+  /* animation: linear 20000ms infinite ${rotate}; */
+  transition: transform 300ms ease;
+
+  .face {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    border: 1px solid black;
+  }
+
+  .face1 {
+    transform: rotateY(0deg) translateZ(${size / 2}px);
+  }
+
+  .face2 {
+    transform: rotateY(90deg) translateZ(${size / 2}px);
+  }
+
+  .face3 {
+    transform: rotateY(180deg) translateZ(${size / 2}px);
+  }
+
+  .face4 {
+    transform: rotateY(-90deg) translateZ(${size / 2}px);
+  }
+
+  .face5 {
+    transform: rotateX(90deg) translateZ(${size / 2}px);
+  }
+
+  .face6 {
+    transform: rotateX(-90deg) translateZ(${size / 2}px);
+  }
 `;
