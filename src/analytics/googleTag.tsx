43a7f4640a82dbd8e552f55b44f1ea4d74b3248f
richhdev <79666708+richhdev@users.noreply.github.com>
2023-04-30 00:15:32 +1000

    :chart_with_upwards_trend: update google analytics tag

--- src/analytics/googleTag.tsx
+++ src/analytics/googleTag.tsx
@@ -1,13 +1,12 @@
 const googleTag = `
   <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-DVQMDEHD24"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-DVXSCKNET4"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
-    gtag('config', 'G-DVQMDEHD24');
-  </script>
-`;
+  
+    gtag('config', 'G-DVXSCKNET4');
+  </script>`;
 
 export default googleTag;
