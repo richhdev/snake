5ca1001f6384a54c5d0028651ac446995461572c
richhdev <79666708+richhdev@users.noreply.github.com>
2023-04-30 00:13:15 +1000

    :chart_with_upwards_trend: update google analytics tag

--- src/analytics/googleTag.tsx
+++ src/analytics/googleTag.tsx
@@ -1,12 +1,13 @@
 const googleTag = `
   <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-DVXSCKNET4"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-DVQMDEHD24"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-  
-    gtag('config', 'G-DVXSCKNET4');
-  </script>`;
+
+    gtag('config', 'G-DVQMDEHD24');
+  </script>
+`;
 
 export default googleTag;
